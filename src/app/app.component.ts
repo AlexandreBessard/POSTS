9fc29bea628ada7de3ad7e78b5adfd865b032032
Alexandre <alexbox75@hotmail.fr>
2019-02-21 15:32:14 +0100

    final Post project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Post} from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+
+  postTitle = 'Posts';
+
+  postListItem: Post[] = [
+
+    new Post("Mon premier post",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      0,
+      new Date()),
+
+    new Post("Mon deuxième post",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      0,
+      new Date()),
+
+    new Post("Encore un post",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      0,
+      new Date()
+    )
+  ];
+
+
 }
